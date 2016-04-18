87d0359ab1c56ed2a6271f48be8aef151a9f065e
akm <akm2000@gmail.com>
2016-04-18 11:42:41 +0900

    Define methods for omniouth

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,20 @@ class User < ActiveRecord::Base
         User.current_user = orig_user
       end
     end
+
+    def from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(:email => data["email"]).first
+
+      # Uncomment the section below if you want users to be created if they don't exist
+      # unless user
+      #     user = User.create(name: data["name"],
+      #        email: data["email"],
+      #        password: Devise.friendly_token[0,20]
+      #     )
+      # end
+      user
+    end
   end
 
 end
