02ca141282af8612adaa2cff243f10db091ecbe7
akm <akm2000@gmail.com>
2016-04-18 11:23:56 +0900

    Add settings of timezone, locale and generators

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,19 @@ Bundler.require(*Rails.groups)
 
 module PgDeviseOmniauthGoogleOauth2
   class Application < Rails::Application
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      # g.orm             :mongoid
+      g.test_framework  :rspec
+      g.factory_girl dir: 'spec/factories'
+      # g.template_engine :haml
+    end
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
