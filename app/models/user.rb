30a25a028a35036683ffd6eb192d9d03af0b6dde
akm <akm2000@gmail.com>
2016-04-18 11:43:34 +0900

    Uncomment the section to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,13 +27,13 @@ class User < ActiveRecord::Base
       data = access_token.info
       user = User.where(:email => data["email"]).first
 
-      # Uncomment the section below if you want users to be created if they don't exist
-      # unless user
-      #     user = User.create(name: data["name"],
-      #        email: data["email"],
-      #        password: Devise.friendly_token[0,20]
-      #     )
-      # end
+      # To be created if they don't exist
+      unless user
+          user = User.create(name: data["name"],
+             email: data["email"],
+             password: Devise.friendly_token[0,20]
+          )
+      end
       user
     end
   end
